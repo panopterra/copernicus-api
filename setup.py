c17b538bc4e8813f44ffc73b21bf77e995757ee5
Matthias P. Wagner <matthias.p.wagner@gmail.com>
2024-09-11 13:54:37 +0200

    setup.py: additional classifiers, requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import find_packages, setup
 
+# load README.md as long description for PyPI
 with open('README.md', 'r') as f:
     long_description = f.read()
 
+# run setup
 setup(name='copernicusapi',
-      version='0.0.1',
+      version='0.0.7',
       description='A package to facilitate interactive construction of queries to the Copernicus Data Space Ecosystem repository.',
       package_dir={'copernicusapi': 'copernicusapi'},
       packages=find_packages(),
@@ -18,15 +20,25 @@ setup(name='copernicusapi',
           'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python :: 3',
           'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'Topic :: Scientific/Engineering :: GIS',
+          'Topic :: Utilities'
       ],
       keywords='copernicus, data space, esa, satellite, download, remote sensing, earth observation',
-      install_requires=['shapely>=2.0',
+      install_requires=['geopandas>=0.14',
                         'numpy>=1.26',
                         'pandas>=2.0',
-                        'geopandas>=0.14',
-                        'pyproj>=3.6'],
+                        'pyproj>=3.6',
+                        'shapely>=2.0',
+                        ],
       extras_require={
-          'dev': ['pytest>=8.0', 'twine>=5.1']
-      },
+          'dev': ['black>=',
+                  'flake>=7.0',
+                  'pytest-cov>=4.1'
+                  'twine>=5.1',
+                  'pytest>=8.0']
+                  },
       python_requires='>=3.10'
       )
